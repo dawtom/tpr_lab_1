84a9ab959000650b50fcc17391015ecd076c57f1
tomasiew <tomasiew@vnode-01.dydaktyka.icsr.agh.edu.pl>
2019-03-08 11:26:47 +0100

    a

--- ssend.c
+++ ssend.c
@@ -14,23 +14,27 @@ int main(int argc, char** argv) {
         fprintf(stderr, "World size must be greater than 1 for %s\n", argv[0]);
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
-    char * text = malloc(1000000 * sizeof(char));
+    char * text = malloc(2000000 * sizeof(char));
 //  SENDER
     if (world_rank == 0) {
         int i;
-	for (i = 0; i < 10000; i++) {
+	for (i = 0; i < 20000; i++) {
             strcat(text, "Miala baba koguta, wsadzila go do butaMiala baba koguta, wsadzila go do butaMiala baba koguta");
         }
+	printf("Sending");
         t1 = MPI_Wtime();
-        MPI_Ssend(text, 1000000, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
-        MPI_Barrier(MPI_COMM_WORLD);
+        MPI_Ssend(text, 2000000, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+        printf("Before barrier");
+	//MPI_Barrier(MPI_COMM_WORLD);
+	printf("After barrier");
         t2 = MPI_Wtime();
-        printf("Time: %1.2f\n", t2-t1);
+	//printf("time1: %f \ntime2: %f\n", t1, t2);
+        printf("Time: %1.6f\n", t2-t1);
     } else
 //    RECEIVER
     if (world_rank == 1) {
-        MPI_Recv(text, 1000000, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Barrier(MPI_COMM_WORLD);
+        MPI_Recv(text, 2000000, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        //MPI_Barrier(MPI_COMM_WORLD);
         printf("Process 1 received text from process 0\n");
     }
     MPI_Finalize();
